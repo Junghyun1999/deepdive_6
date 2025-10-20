dfe6a54d516608293323707ae01c574c20c34caa
junghyunyoo <yoojh4539@gmail.com>
2025-10-20 11:52:40 +0900

    1020_01

---
+++ 1020_01.js
@@ -0,0 +1,29 @@
+// Cmd + J -> 터미널 열기
+// console.log("hello"); 
+
+const a = 10;
+const b = "10";
+const c = true;
+const d = 3;
+
+console.log(typeof a,a); //number
+console.log(typeof b); //string
+console.log(typeof c); //boolean
+
+// 산술연산자
+console.log(a + d);
+console.log(a * d);
+console.log(a / d);
+console.log(a - d);
+console.log(a % d); //나머지연산자
+
+//비교연산자
+console.log(a == b) // true -> 값만비교/데이터타입은 비교하지 않는다.
+console.log(a != b)
+console.log(a > b)
+console.log(a >= b)
+console.log(a < b)
+console.log(a <= b)
+console.log(a === b) //false -> 값과 데이터타입을 모두 비교
+console.log(a !== b)
+
